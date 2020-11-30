e65e1151a0e8b4bca6bf52cea96f6643eff8f965
Ubuntu <ubuntu@ip-172-31-76-11.ec2.internal>
2020-11-30 18:48:08 +0000

    This program calculates addition

---
+++ addition_calculator.py
@@ -0,0 +1,22 @@
+# 1/usr/bin/env python3
+# Created by Larry Nkengbeza
+# Created on November 2020
+# This program calculates addition
+
+def main():
+    # this program calculates addition
+
+    # Input
+    a = int(input("Enter the first  number to be calculated:"))
+    b = int(input("Enter the  second number to be calculated:"))
+
+    # Process
+    addition = a+b
+
+    # Output
+    print("a+b=")
+    print("Answer is {}".format(addition))
+
+
+if __name__ == "__main__":
+    main()
